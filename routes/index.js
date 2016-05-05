f1cf924ab751cdd79ca030d04c2ef85a7c72031a
Todd Kenna <i.toddicus@gmail.com>
2016-05-05 17:30:43 -0500

    Bug fixed, maybe

--- routes/index.js
+++ routes/index.js
@@ -195,6 +195,7 @@ router.patch('/children/:hash/checkin', function(req, res, next){
 /* list checkins */
 router.get('/children/:hash/review', authenticatedUser, function(req, res, next){
   Child.find({ url: req.params.hash }, 'fname lname url checkins', function(err, child) {
+    if(child[0].checkins[0] != undefined && child[0].checkins[0].lat != null){
       res.render('review', {
         fname: child[0].fname,
         lname: child[0].lname,
@@ -203,14 +204,16 @@ router.get('/children/:hash/review', authenticatedUser, function(req, res, next)
         long: child[0].checkins[0].long,
         checkins: child[0].checkins
       });
+    };
   });
+   res.redirect('/home');
 });
 
 /*QR code generation */
 router.get('/children/:hash/qr', authenticatedUser, function(req, res) { 
   Child.find({ url: req.params.hash }, 'fname lname url', function(err, child) {
-     var urlA = "https://gacheckins.herokuapp.com/children/"
-     // var urlA = "http://localhost:3000/children/"
+     // var urlA = "https://gacheckins.herokuapp.com/children/"
+     var urlA = "http://localhost:3000/children/"
     var myUrl = urlA.concat(child[0].url) + "/checkin";
     console.log(myUrl);
     var code = qr.image(myUrl, { type: 'svg' });
